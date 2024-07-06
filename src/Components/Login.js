56f8363b3d5cd4c459d0c3283e61282bfeeb4c2f
Mohammed Parves <mdperrfan@gmail.com>
2024-07-06 23:26:28 +0600

    Designed Login and Register and used animation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,9 +4,12 @@ import { auth } from "../Config/firebase";
 import { Link } from 'react-router-dom'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import Swal from "sweetalert2"
+import AnimatedPage from "./AnimatedPage";
+import './auth.css';
 export default function Login() {
     const [credentials, setCredentials] = useState({ email: "", password: "" });
     const navigate = useNavigate();
+
     const handleLogin = async (e) => {
         e.preventDefault();
         const { email, password } = credentials;
@@ -17,46 +20,55 @@ export default function Login() {
             navigate("/");
         } catch (error) {
             console.log(error.message);
-            Swal.fire("Error..", error.message,"error");
+            Swal.fire("Error..", error.message, "error");
         }
     }
-    const onChange=(e)=>{
-        setCredentials({...credentials,[e.target.name]:e.target.value})
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
     return (
-        <div className="container my-5">
-            <form onSubmit={handleLogin}>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input 
-                    type="email" 
-                    name="email"
-                    className="form-control" 
-                    id="exampleInputEmail1" 
-                    aria-describedby="emailHelp"
-                    value={credentials.email}
-                    onChange={onChange}
-                    required
-                     />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input 
-                    type="password" 
-                    name="password"
-                    className="form-control" 
-                    id="exampleInputPassword1"
-                    value={credentials.password}
-                    onChange={onChange}
-                    required
-                     />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-                <p className="forgot-password text-right">
-                    Don't have an account? <Link to="/register">Register</Link>
-                </p>
-            </form>
+        <AnimatedPage>
+        <div class="login-page">
+            <div class="form">
+                <form className="login-form" onSubmit={handleLogin}>
+                    <header class="head-form">
+                        <h2>Log In</h2>
+                        <p>login here using your email and password</p>
+                    </header>
+                    <div className="mb-3">
+                        {/*                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+ */}                    <input
+                            type="email"
+                            name="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="email"
+                            value={credentials.email}
+                            onChange={onChange}
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        {/*                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+ */}                    <input
+                            type="password"
+                            name="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder="password"
+                            value={credentials.password}
+                            onChange={onChange}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                    <p className="message">
+                        Don't have an account? <Link to="/register">Register</Link>
+                    </p>
+                </form>
+            </div>
         </div>
+        </AnimatedPage>
     )
 }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,11 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
-import { auth} from "../Config/firebase";
-import { setDoc, doc,getFirestore } from "firebase/firestore";
+import { auth } from "../Config/firebase";
+import { setDoc, doc, getFirestore } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import './auth.css'
+import AnimatedPage from "./AnimatedPage";
 export default function Register() {
     const [credentials, setCredentials] = useState({ Fname: "", Lname: "", Sid: "", email: "", password: "" });
 
@@ -17,25 +19,25 @@ export default function Register() {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const user = userCredential.user; // Use userCredential for the user object
             console.log(user);
-        
+
             if (user) {
                 const db = getFirestore(auth.app);
-              // Create a reference to the user document using the user ID
-              const userRef = doc(db, "users", user.uid); // db is likely your Firestore instance
-        
-              await setDoc(userRef, {
-                email: user.email,
-                firstName: Fname,
-                lastName: Lname,
-                studentId: Sid,
-                userType: "user"
-              });
-              }
-              console.log("User Registered Successfully!!");
-              Swal.fire("Success", "User Registered Successfully!!", "success");
+                // Create a reference to the user document using the user ID
+                const userRef = doc(db, "users", user.uid); // db is likely your Firestore instance
+
+                await setDoc(userRef, {
+                    email: user.email,
+                    firstName: Fname,
+                    lastName: Lname,
+                    studentId: Sid,
+                    userType: "user"
+                });
+            }
+            console.log("User Registered Successfully!!");
+            Swal.fire("Success", "User Registered Successfully!!", "success");
         } catch (error) {
             console.log(error.message);
-            Swal.fire("Error..",error.message,"error")
+            Swal.fire("Error..", error.message, "error")
         }
     }
 
@@ -43,78 +45,77 @@ export default function Register() {
         setCredentials({ ...credentials, [e.target.name]: e.target.value });
     }
     return (
-        <div className="container my-5">
-            <form onSubmit={handleRegister}>
-                <h3>Sign Up</h3>
-                <div className="mb-3">
-                    <label>First name</label>
-                    <input
-                        name="Fname"
-                        type="text"
-                        className="form-control"
-                        placeholder="First name"
-                        value={credentials.Fname}
-                        onChange={onChange}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Last name</label>
-                    <input
-                        name="Lname"
-                        type="text"
-                        className="form-control"
-                        placeholder="Last name"
-                        value={credentials.Lname}
-                        onChange={onChange}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Student ID</label>
-                    <input
-                        name="Sid"
-                        type="text"
-                        className="form-control"
-                        placeholder="Student ID"
-                        value={credentials.Sid}
-                        onChange={onChange}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Email address</label>
-                    <input
-                        name="email"
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                        value={credentials.email}
-                        onChange={onChange}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                        value={credentials.password}
-                        onChange={onChange}
-                        required
-                    />
-                </div>
+        <AnimatedPage>
+            <div class="login-page">
+                <div class="form">
+                    <form className="register-form" onSubmit={handleRegister}>
+                        <h3>Sign Up</h3>
+                        <div className="mb-3">
+                            <input
+                                name="Fname"
+                                type="text"
+                                className="form-control"
+                                placeholder="First name"
+                                value={credentials.Fname}
+                                onChange={onChange}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                name="Lname"
+                                type="text"
+                                className="form-control"
+                                placeholder="Last name"
+                                value={credentials.Lname}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                name="Sid"
+                                type="text"
+                                className="form-control"
+                                placeholder="Student ID"
+                                value={credentials.Sid}
+                                onChange={onChange}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                name="email"
+                                type="email"
+                                className="form-control"
+                                placeholder="Enter email"
+                                value={credentials.email}
+                                onChange={onChange}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                name="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter password"
+                                value={credentials.password}
+                                onChange={onChange}
+                                required
+                            />
+                        </div>
 
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                        Sign Up
-                    </button>
+                        <div className="d-grid">
+                            <button type="submit" className="btn btn-primary">
+                                Sign Up
+                            </button>
+                        </div>
+                        <p className="message">
+                            Already registered <Link to="/">Login</Link>
+                        </p>
+                    </form>
                 </div>
-                <p className="forgot-password text-right">
-                    Already registered <Link to="/">Login</Link>
-                </p>
-            </form>
-        </div>
+            </div>
+        </AnimatedPage>
     )
 }
